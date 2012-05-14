1664a4a0b1314d471366d9786b42c087b299aa90
mfs409@gmail.com <mfs409@gmail.com@67788deb-1e08-3510-293a-186ae70140c8>
2012-05-14 03:10:34 +0000

    removed gen folder since cleaning in eclipse keeps angering svn, and switched to google API to see if that fixes concerns with 2.3.4 androids communicating with the ADK.

--- trunk/controller-project/src/edu/lehigh/cse/paclab/carbot/WalkablePathActivity.java
+++ trunk/controller-project/src/edu/lehigh/cse/paclab/carbot/WalkablePathActivity.java
@@ -6,7 +6,6 @@ import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.LinearLayout;
 
 public class WalkablePathActivity extends Activity
 {
