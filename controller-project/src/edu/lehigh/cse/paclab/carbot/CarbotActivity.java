e5fed3ec265a274b3bcf80cbfc7df3dee9e2d622
mfs409 <mfs409@gmail.com>
2012-05-17 15:08:00 +0000

    added some notes

--- controller-project/src/edu/lehigh/cse/paclab/carbot/CarbotActivity.java
+++ controller-project/src/edu/lehigh/cse/paclab/carbot/CarbotActivity.java
@@ -41,4 +41,91 @@ public class CarbotActivity extends Activity
             startActivity(new Intent(this, edu.lehigh.cse.paclab.prelims.DemosActivity.class));
         }
     }
+
+    /*
+     * Plan from here:
+     * 
+     *   - There will be many different behaviors that we want the robot to support
+     *   
+     *     - Simple control via buttons
+     *       - This will require two phones, one to talk to the other.
+     *       
+     *       - We will want the buttons to make the phone move, and we will also want a 'snap picture' button
+     *       
+     *       - Key challenge here: is our BlueTooth code hardened, or else is WiFi going to work?
+     *       
+     *     - Simple control by talking
+     *     
+     *       - Basically the same as the previous project
+     *       
+     *       - Speech recognition for kids is really hard
+     *       
+     *       - Controlling phone needs to have internet access for SpeechToText API to work
+     *       
+     *     - Control by drawing on the screen
+     *     
+     *       - This will require one phone mounted nicely on the robot
+     *       
+     *       - Which drawing mechanism do we prefer here?
+     *       
+     *       - Calibration is going to be very important
+     *       
+     *       - Need to harden this wrt cancelling motion and resetting
+     *       
+     *     - Game mode: find the ball
+     *     
+     *       - This will require one phone
+     *       
+     *       - First requirement is to teach the phone which ball to look for.  I think the histograms will help.
+     *       
+     *       - Second is to integrate vision code with robot control so that we can move around until we find the ball
+     *       
+     *       - Stopping is going to be a challenge
+     *       
+     *   - Key underlying features
+     *   
+     *     - TextToSpeech
+     *     
+     *       - We want the robot to talk as much as possible
+     *       
+     *     - Hardened ArduinoService
+     *     
+     *       - So that we can register with a robot once
+     *       
+     *         - See http://stackoverflow.com/questions/2621395/more-efficient-way-of-updating-ui-from-service-than-intents/2622473#2622473, 
+     *           or else use an AIDL to create the connection to the service (http://www.helloandroid.com/tutorials/musicdroid-audio-player-part-ii)
+     *       
+     *     - Hardened Phone2Phone communication
+     *     
+     *       - Bluetooth or Wifi... doesn't matter
+     *       
+     *       - Key thing is that we need a connection that won't drop
+     *       
+     *     - Calibration support
+     *     
+     *       - Need to calibrate the Arduino controller so that we get speed and angles right.
+     *       
+     *       - Need to verify that the alarmservice resolution is appropriate... otherwise we'll need to have threads in spinloops
+     *       
+     *       - Need to calibrate the camera to the ball being searched, based on lighting
+     *       
+     *       
+     *   - Update on histogramming from John Spletzer
+     *   
+     *       I wouldn't do histograms, but you can do what you want. I think the easiest thing would be to:
+     *       
+     *         1.  Take a patch that you know will contain portions of the ball
+     *         
+     *         2.  Calculate a mean & std dev for each channel (H-S-V) from the patch
+     *         
+     *         3.  Establish min & max values for each channel (e.g., mean +/- 3 sigma).  This is your color model.
+     *         
+     *         4.  Over each pixel in the new image do:
+     *         
+     *             Ball = H_val>H_min && H_val<H_max && S_val>S_min && S_val<S_max && V_val>V_min && V_val<V_max;
+     *             
+     *       There are ways to speed this up using LUTs, etc., but that's another story.  I'll warn you though that 
+     *       since the camera you are using will likely dynamically adjust brightness, exposure, gain, shutter, etc., 
+     *       that a simple technique may not give great results regardless.  However, I'd start here. 
+     */
 }
