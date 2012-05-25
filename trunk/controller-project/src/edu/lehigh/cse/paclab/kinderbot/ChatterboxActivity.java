541a3f85b3fcda14036079eafbe82a5c8833de05
mfs409@gmail.com <mfs409@gmail.com@67788deb-1e08-3510-293a-186ae70140c8>
2012-05-25 04:23:14 +0000

    set up structure for remaining work

---
+++ trunk/controller-project/src/edu/lehigh/cse/paclab/kinderbot/ChatterboxActivity.java
@@ -0,0 +1,13 @@
+package edu.lehigh.cse.paclab.kinderbot;
+
+/**
+ * This is going to be a silly speech-to-text game. We're going to let the kids
+ * talk to the robot, and see if it can understand their words at all :)
+ * 
+ * [TODO] It would be nice to use Bluetooth as a remote control, for launching
+ * Speech-to-Text intents...
+ */
+public class ChatterboxActivity
+{
+
+}
