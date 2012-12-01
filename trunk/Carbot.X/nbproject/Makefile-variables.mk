e28b0f5f3d9685cd885be7c1893bd6a6f94ae41f
rmnshariati@gmail.com <rmnshariati@gmail.com@67788deb-1e08-3510-293a-186ae70140c8>
2012-12-01 19:23:44 +0000

    Entire MPLABX project for pic added

---
+++ trunk/Carbot.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=Carbot.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/Carbot.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=carbot.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/carbot.x.tar
